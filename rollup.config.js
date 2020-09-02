01ef4c6d3735f8187797d3e0d89abd0b06d56235
Jeffrey Heer <jheer@trifacta.com>
2020-09-02 11:58:15 +0200

    chore: Update build targets.

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,25 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import bundleSize from 'rollup-plugin-bundle-size';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.js',
-  plugins: [ nodeResolve({ modulesOnly: true }) ],
+  plugins: [ nodeResolve({ modulesOnly: true }), bundleSize() ],
   output: [
     {
-      file: 'build/arquero.js',
+      file: 'dist/arquero.js',
       name: 'aq',
       format: 'umd'
     },
     {
-      file: 'build/arquero.min.js',
+      file: 'dist/arquero.min.js',
       name: 'aq',
       format: 'umd',
+      sourcemap: true,
       plugins: [ terser({ ecma: 2018 }) ]
     },
     {
-      file: 'build/arquero.mjs',
+      file: 'dist/arquero.mjs',
       format: 'es'
     }
   ]
