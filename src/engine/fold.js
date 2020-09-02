6f46ee09f58a15567a95c1622853bfe59743765e
Jeffrey Heer <jheer@trifacta.com>
2020-09-02 10:46:48 +0200

    feat: Update fold syntax.

--- src/engine/fold.js
+++ src/engine/fold.js
@@ -1,17 +1,18 @@
 import unroll from './unroll';
 import { aggregateGet } from './reduce/util';
 
-export default function(table, { values = {}, ops = [] }, options) {
+export default function(table, { values = {}, ops = [] }, options = {}) {
   const keys = Object.keys(values);
   const n = keys.length;
   if (n === 0) return table;
 
+  const as = options.as || [];
   const vals = aggregateGet(table, ops, Object.values(values));
   const exprs = {
     drop: values,
     values: {
-      key: () => keys,
-      value: (row, data) => vals.map(fn => fn(row, data))
+      [as[0] || 'key']: () => keys,
+      [as[1] || 'value']: (row, data) => vals.map(fn => fn(row, data))
     }
   };
 
--- test/verbs/fold-test.js
+++ test/verbs/fold-test.js
@@ -1,20 +1,37 @@
 import tape from 'tape';
 import tableEqual from '../table-equal';
-import { table } from '../../src/verbs';
+import { not, table } from '../../src/verbs';
 
-tape('fold generates key-value pair columns', t => {
-  const data = {
+function data() {
+  return {
     k: ['a', 'b', 'b'],
     x: [1, 2, 3],
     y: [9, 8, 7]
   };
+}
 
-  const ut = table(data).fold('x', 'y');
-
-  tableEqual(t, ut, {
+function output(key = 'key', value = 'value') {
+  return {
     k: ['a', 'a', 'b', 'b', 'b', 'b'],
-    key: ['x', 'y', 'x', 'y', 'x', 'y'],
-    value: [1, 9, 2, 8, 3, 7]
-  }, 'fold data');
+    [key]: ['x', 'y', 'x', 'y', 'x', 'y'],
+    [value]: [1, 9, 2, 8, 3, 7]
+  };
+}
+
+tape('fold generates key-value pair columns', t => {
+  const ut = table(data()).fold(['x', 'y']);
+  tableEqual(t, ut, output(), 'fold data');
+  t.end();
+});
+
+tape('fold accepts select statements', t => {
+  const ut = table(data()).fold(not('k'));
+  tableEqual(t, ut, output(), 'fold selected data');
+  t.end();
+});
+
+tape('fold accepts named output columns', t => {
+  const ut = table(data()).fold(['x', 'y'], { as: ['u', 'v'] });
+  tableEqual(t, ut, output('u', 'v'), 'fold as data');
   t.end();
 });
