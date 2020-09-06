b3e56a6712a05d2cccc49882c382e9f75354e158
Jeffrey Heer <jheer@trifacta.com>
2020-09-06 22:45:15 +0200

    feat: Export random seed method.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ export const internal = {
 
 // export public API
 export { version } from '../package.json';
+export { seed } from './util/random';
 export { default as fromArrow } from './format/from-arrow';
 export { default as fromCSV } from './format/from-csv';
 export { default as fromJSON } from './format/from-json';
--- src/util/random.js
+++ src/util/random.js
@@ -1,14 +1,31 @@
+import isValid from './is-valid';
+
 let source = Math.random;
 
 export function random() {
   return source();
 }
 
+/**
+ * Set a seed value for random number generation.
+ * If the seed is a valid number, a 32-bit linear congruential generator
+ * with the given seed will be used to generate random values.
+ * If the seed is null, undefined, or not a valid number, the random
+ * number generator will revert to Math.random.
+ * @param {number} seed The random seed value. Should either be an
+ *  integer or a fraction between 0 and 1.
+ */
 export function seed(seed) {
-  const m = 2147483647; // 2^31 - 1
-  const a = 1103515245;
-  const c = 12345;
+  source = isValid(seed) && isFinite(seed = +seed) ? lcg(seed) : Math.random;
+}
+
+function lcg(seed) {
+  const a = 0x19660D;
+  const c = 0x3C6EF35F;
+  const m = 1 / 0x100000000;
+  seed = (0 <= seed && seed < 1 ? seed / m : Math.abs(seed)) | 0;
+
   // Random numbers using a Linear Congruential Generator with seed value
   // https://en.wikipedia.org/wiki/Linear_congruential_generator
-  source = () => (seed = (a * seed + c) % m) / m;
+  return () => (seed = a * seed + c | 0, m * (seed >>> 0));
 }
