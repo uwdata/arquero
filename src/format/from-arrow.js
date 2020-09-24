5530466939398f5616082ea557ffcf6c0e8d3669
jheer <jeffrey.heer@gmail.com>
2020-09-24 12:08:46 +0200

    perf: Use efficient Arrow dictionary unpacking.

--- src/format/from-arrow.js
+++ src/format/from-arrow.js
@@ -35,6 +35,39 @@ export default function(arrowTable, options = {}) {
 }
 
 function arrayFromArrow(column) {
+  // if dictionary column, perform more efficient extraction
   // if has null values, extract to standard array
-  return column.nullCount > 0 ? [...column] : column.toArray();
+  // otherwise, let Arrow try to use copy-less subarray call
+  return column.dictionary ? arrayFromDictionary(column)
+    : column.nullCount > 0 ? [...column]
+    : column.toArray();
+}
+
+function arrayFromDictionary(column) {
+  // decode utf-8 only once per dictionary key
+  // use the last chunk in case the dictionary builds as it goes
+  const chunks = column.chunks;
+  const dict = chunks[chunks.length - 1].dictionary.toArray();
+  const array = new Array(column.length);
+
+  // populate array values
+  let i = -1;
+  for (const chunk of chunks) {
+    const { nullBitmap: nulls, data: { values, length: m } } = chunk;
+    if (nulls && nulls.length) {
+      for (let j = 0; j < m; ++j) {
+        // j >> 3 advances the byte every 8 bits;
+        // (1 << (j & 7) checks if the relevant bit is set in that byte
+        array[++i] = !(nulls[j >> 3] & (1 << (j & 7)))
+          ? null
+          : dict[values[j]];
+      }
+    } else {
+      for (let j = 0; j < m; ++j) {
+        array[++i] = dict[values[j]];
+      }
+    }
+  }
+
+  return array;
 }
--- test/format/arrow-test.js
+++ test/format/arrow-test.js
@@ -1,7 +1,7 @@
 import tape from 'tape';
 import fromArrow from '../../src/format/from-arrow';
 
-// test stub for Arrow Column API
+// test stubs for Arrow Column API
 function arrowColumn(data, nullCount = 0) {
   return {
     length: data.length,
@@ -9,8 +9,45 @@ function arrowColumn(data, nullCount = 0) {
     toArray: () => data,
     [Symbol.iterator]: () => data[Symbol.iterator](),
     nullCount,
-    data
+    _data: data
   };
+}
+
+function arrowDictionary(data) {
+  let key = -1;
+  let nullCount = 0;
+  const bitmap = new Uint8Array(Math.ceil(data.length / 8)).fill(0xFF);
+  const lut = {};
+  const dict = [];
+  const keys = data.map((v, i) => {
+    if (v == null) {
+      ++nullCount;
+      bitmap[i >> 3] = bitmap[i >> 3] & ~(1 << (i & 7));
+      return 0;
+    }
+    if (lut[v] == null) {
+      lut[v] = ++key;
+      dict[key] = v;
+      return key;
+    } else {
+      return lut[v];
+    }
+  });
+
+  const column = {
+    length: data.length,
+    get: row => data[row],
+    toArray: () => data,
+    [Symbol.iterator]: () => data[Symbol.iterator](),
+    dictionary: { toArray: () => dict },
+    nullCount,
+    nullBitmap: nullCount ? bitmap : null,
+    data: { values: keys, length: data.length },
+    _data: data
+  };
+
+  column.chunks = [ column ];
+  return column;
 }
 
 // test stub for Arrow Table API
@@ -36,11 +73,15 @@ tape('fromArrow imports Apache Arrow tables', t => {
 tape('fromArrow can unpack Apache Arrow tables', t => {
   const u = arrowColumn([1, 2, 3, 4, 5]);
   const v = arrowColumn(['a', 'b', null, 'd', 'e'], 1);
-  const at = arrowTable({ u, v });
+  const x = arrowDictionary(['cc', 'dd', 'cc', 'dd', 'cc']);
+  const y = arrowDictionary(['aa', 'aa', null, 'bb', 'bb']);
+  const at = arrowTable({ u, v, x, y });
   const dt = fromArrow(at, { unpack: true });
 
-  t.notDeepEqual(dt.data(), { u, v }, 'unpack to new columns');
-  t.equal(dt.column('u').data, u.data, 'reuse column data without nulls');
-  t.notEqual(dt.column('v').data, u.data, 'copy column data with nulls');
+  t.notDeepEqual(dt.data(), { u, v, x, y }, 'unpack to new columns');
+  t.equal(dt.column('u').data, u._data, 'reuse column data without nulls');
+  t.notEqual(dt.column('v').data, u._data, 'copy column data with nulls');
+  t.deepEqual(dt.column('x').data, x._data, 'unpack dictionary column without nulls');
+  t.deepEqual(dt.column('y').data, y._data, 'unpack dictionary column with nulls');
   t.end();
 });
