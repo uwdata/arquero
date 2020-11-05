0a1c14137d8c11ed0be6a652d163d92a4e18a9c3
jheer <jeffrey.heer@gmail.com>
2020-11-05 13:16:56 +0100

    chore: Update comment formatting.

--- src/format/to-json.js
+++ src/format/to-json.js
@@ -9,7 +9,7 @@ import isDate from '../util/is-date';
  * @property {string[]|Function} [columns] Ordered list of column names
  *  to include. If function-valued, the function should accept a table as
  *  input and return an array of column name strings.
-* @property {Object} [format] Object of column format options. The object
+ * @property {Object} [format] Object of column format options. The object
  *  keys should be column names. The object values should be formatting
  *  functions to invoke to transform column values prior to output. If
  *  specified, these override any automatically inferred options.
