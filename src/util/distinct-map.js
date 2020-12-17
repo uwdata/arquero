16d137f7d507d9952f5ce348332acfdc207889a3
jheer <jeffrey.heer@gmail.com>
2020-12-17 17:06:31 +0100

    fix: Count distinct should group dates with same timestamp.

---
+++ src/util/distinct-map.js
@@ -0,0 +1,30 @@
+import isDate from './is-date';
+
+function key(value) {
+  return isDate(value) ? '#date:' + +value : value;
+}
+
+export default function() {
+  const map = new Map();
+  return {
+    count() {
+      return map.size;
+    },
+    values() {
+      return Array.from(map.values(), _ => _.v);
+    },
+    increment(v) {
+      const k = key(v);
+      const e = map.get(k);
+      e ? ++e.n : map.set(k, { v, n: 1 });
+    },
+    decrement(v) {
+      const k = key(v);
+      const e = map.get(k);
+      e.n === 1 ? map.delete(k) : --e.n;
+    },
+    forEach(fn) {
+      map.forEach(({ v, n }) => fn(v, n));
+    }
+  };
+}
