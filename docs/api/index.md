5d5848632c365e0a190be64ce9c02aa7523fc116
jheer <jeffrey.heer@gmail.com>
2020-12-17 13:41:39 +0100

    feat: Support filtered Arrow tables.

--- docs/api/index.md
+++ docs/api/index.md
@@ -83,12 +83,16 @@ aq.from(new Map([ ['d', 4], ['e', 5], ['f', 6] ])
 <hr/><a id="fromArrow" href="#fromArrow">#</a>
 <em>aq</em>.<b>fromArrow</b>(<i>arrowTable</i>[, <i>options</i>]) · [Source](https://github.com/uwdata/arquero/blob/master/src/format/from-arrow.js)
 
-Create a new <a href="table">table</a> backed by an [Apache Arrow](https://arrow.apache.org/) table instance.
+Create a new <a href="table">table</a> backed by an [Apache Arrow](https://arrow.apache.org/docs/js/) table instance. Both standard and filtered tables are supported. If the input Arrow table is filtered, new column arrays are created and filled with unpacked, non-filtered values.
 
- * *arrowTable*: An Apache Arrow data table.
- * *options*: An Arrow import options object:
-   * *columns*: Ordered list of column names to include.
-   * *unpack*: A boolean flag (default `false`) to unpack binary-encoded Arrow data to standard JavaScript values. Unpacking can incur an upfront time and memory cost to extract data to new arrays, but can speed up later query processing by enabling faster data access.
+For primitive data types, by default Arquero uses the binary-encoded Arrow columns as-is with zero data copying. Nested list or struct types are always unpacked into JavaScript array or object instances, respectively.
+
+The *unpack* option determines if Arrow data should be "unpacked" from binary format to standard JavaScript values. By default, Arrow columns for non-nested data types are used directly. This avoids data copies, but can result in slower access paths, particularly for dictionary columns and strings that need to be extracted from a backing buffer. With *unpack* set to `true`, Arquero performs extraction up front, increasing initialization time and memory use, but enabling faster performance for subsequent queries.
+
+* *arrowTable*: An [Apache Arrow](https://arrow.apache.org/docs/js/) data table.
+* *options*: An Arrow import options object:
+  * *columns*: Ordered list of column names to include.
+  * *unpack*: A boolean flag (default `false`) to unpack binary-encoded Arrow data to standard JavaScript values. Unpacking can incur an upfront time and memory cost to extract data to new arrays, but can speed up later query processing by enabling faster data access.
 
 *Examples*
 
@@ -104,12 +108,12 @@ aq.fromArrow(await arrow.Table.from(fetch(url)))
 
 Parse a comma-separated values (CSV) *text* string into a <a href="table">table</a>. Delimiters other than commas, such as tabs or pipes ('\|'), can be specified using the *options* argument. By default, automatic type inference is performed for input values; string values that match the ISO standard date format are parsed into JavaScript Date objects. To disable this behavior set *options.autoType* to `false`, which will cause all columns to be loaded as strings. To perform custom parsing of input column values, use *options.parse*.
 
- * *text*: A string in a delimited-value format.
- * *options*: A CSV format options object:
-   * *delimiter*: The delimiter string between column values.
-   * *header*: Boolean flag (default `true`) to specify the presence of a  header row. If `true`, indicates the CSV contains a header row with column names. If `false`, indicates the CSV does not contain a header row and the columns are given the names `'col1'`, `'col2'`, and so on.
-   * *autoType*: Boolean flag (default `true`) for automatic type inference.
-   * *parse*: Object of column parsing options. The object keys should be column names. The object values should be parsing functions to invoke to transform values upon input.
+* *text*: A string in a delimited-value format.
+* *options*: A CSV format options object:
+  * *delimiter*: The delimiter string between column values.
+  * *header*: Boolean flag (default `true`) to specify the presence of a  header row. If `true`, indicates the CSV contains a header row with column names. If `false`, indicates the CSV does not contain a header row and the columns are given the names `'col1'`, `'col2'`, and so on.
+  * *autoType*: Boolean flag (default `true`) for automatic type inference.
+  * *parse*: Object of column parsing options. The object keys should be column names. The object values should be parsing functions to invoke to transform values upon input.
 
 *Examples*
 
--- src/format/from-arrow.js
+++ src/format/from-arrow.js
@@ -2,6 +2,7 @@ import ColumnTable from '../table/column-table';
 import error from '../util/error';
 import toString from '../util/to-string';
 import unroll from '../util/unroll';
+import unroll2 from '../util/unroll2';
 
 // Hardwire Arrow type ids to avoid explicit dependency
 // https://github.com/apache/arrow/blob/master/js/src/enum.ts
@@ -25,89 +26,155 @@ export const STRUCT = 13;
  * @param {ColumnTable} table A new table containing the imported values.
  */
 export default function(arrowTable, options = {}) {
-  const columns = {};
-
   const names = options.columns || arrowTable.schema.fields.map(f => f.name);
-  const unpack = !!options.unpack;
+  const count = arrowTable.count();
 
-  names.forEach(name => {
-    const column = arrowTable.getColumn(name);
-    if (column == null) {
-      error(`Arrow column does not exist: ${toString(name)}`);
-    }
-    columns[name] = column.numChildren ? arrayFromNested(column)
-      : unpack ? arrayFromVector(column)
-      : column;
-  });
+  const cols = names.map(name =>
+    arrowTable.getColumn(name) ||
+    error(`Arrow column does not exist: ${toString(name)}`)
+  );
 
-  return new ColumnTable(columns);
+  const data = arrowTable.length === count
+    ? collect(names, cols, !!options.unpack)
+    : collectFiltered(arrowTable, names, cols, count);
+
+  return new ColumnTable(data, null, null, null, null, names);
 }
 
-function arrayFromNested(vector) {
+function collect(names, cols, unpack) {
+  const data = {};
+
+  cols.forEach((col, idx) =>
+    data[names[idx]] = col.numChildren ? arrayFromNested(col)
+      : unpack ? arrayFromVector(col)
+      : col
+  );
+
+  return data;
+}
+
+function collectFiltered(input, names, cols, count) {
+  const data = {};
+  const out = cols.map((col, idx) => data[names[idx]] = newArray(col, count));
+  const lut = cols.map(col => col.dictionary ? dictionaryLookup(col) : null);
+
+  let row = -1;
+  let batch = -1;
+  let collect;
+
+  input.scan(
+    idx => collect(++row, idx),
+    () => {
+      ++batch;
+      collect = unroll2(
+        out,
+        cols.map((col, i) => extractFrom(col.chunks[batch], lut[i])),
+        ['row', 'idx'],
+        '{' + out.map((_, i) => `_${i}[row]=$${i}(idx)`).join(';') + ';}'
+      );
+    }
+  );
+
+  return data;
+}
+
+// Typed Arrays can be used for the following Arrow types:
+//  Int = 2; Float = 3; Decimal = 7; Time = 9; Timestamp = 10;
+function newArray(col, count) {
+  if (!col.nullCount) {
+    const { ArrayType, typeId: id } = col;
+    if (id === 2 || id === 3 || id === 7 || id === 9 || id === 10) {
+      return new ArrayType(count);
+    }
+  }
+
+  return Array(count);
+}
+
+// extraction function for input vector
+// If dictionary type, lookup function must be provided
+function extractFrom(vector, lookup) {
+  return vector.numChildren ? extractFromNested(vector)
+    : vector.dictionary ? extractFromDictionary(vector, lookup)
+    : idx => vector.get(idx);
+}
+
+// extraction function for nested column types
+function extractFromNested(vector) {
   const extract = vector.typeId === LIST ? i => arrayExtractor(vector.get(i))
     : vector.typeId === STRUCT ? structExtractor(vector)
     : error(`Unsupported Arrow type: ${toString(vector.VectorName)}`);
-
-  // generate and return objects for each nested value
-  return Array.from(
-    { length: vector.length },
-    (_, i) => vector.isValid(i) ? extract(i) : null
-  );
+  return idx => vector.isValid(idx) ? extract(idx) : null;
 }
 
+// extraction function for dictionary-encoded values
+// For null bitmaps:
+//  j >> 3 advances the byte every 8 bits;
+//  (1 << (j & 7) checks if the relevant bit is set in that byte
+function extractFromDictionary(vector, lookup) {
+  const { nullBitmap: nulls, data: { values: keys } } = vector;
+  return nulls && nulls.length
+    ? i => (nulls[i >> 3] & (1 << (i & 7))) ? lookup(keys[i]) : null
+    : i => lookup(keys[i]);
+}
+
+// decode utf-8 only once per dictionary key
+// use the last chunk in case the dictionary builds as it goes
+// perform lazy extraction to avoid processing unused entries
+function dictionaryLookup(chunked) {
+  const chunk = chunked.chunks[chunked.chunks.length - 1];
+  const dict = chunk.dictionary;
+  const values = [];
+  return key => {
+    const v = values[key];
+    return v === undefined ? (values[key] = dict.get(key)) : v;
+  };
+}
+
+// extract from an array-typed vector, recurse if nested.
 function arrayExtractor(vector) {
-  // extract an array, recurse if nested.
   return vector.numChildren
     ? arrayFromNested(vector)
     : arrayFromVector(vector);
 }
 
+// extract struct field names and values
+// return function to generate objects with field name properties
 function structExtractor(vector) {
-  // extract struct field names and values
   const names = vector.type.children.map(field => field.name);
   const values = names.map((_, i) => arrayExtractor(vector.getChildAt(i)));
-
-  // function to generate objects with field name properties
   return unroll(
     values, 'i',
     '({' + names.map((_, d) => `${toString(_)}:_${d}[i]`) + '})'
   );
 }
 
-function arrayFromVector(column) {
-  // if dictionary column, perform more efficient extraction
-  // if has null values, extract to standard array
-  // otherwise, let Arrow try to use copy-less subarray call
-  return column.dictionary ? arrayFromDictionary(column)
-    : column.nullCount > 0 ? [...column]
-    : column.toArray();
+// generate and return objects for each nested value
+function arrayFromNested(vector) {
+  const extract = extractFromNested(vector);
+  return Array.from({ length: vector.length }, (_, i) => extract(i));
 }
 
-function arrayFromDictionary(column) {
-  // decode utf-8 only once per dictionary key
-  // use the last chunk in case the dictionary builds as it goes
-  const chunks = column.chunks;
-  const dict = chunks[chunks.length - 1].dictionary.toArray();
-  const array = new Array(column.length);
+// if dictionary column, perform more efficient extraction
+// if has null values, extract to standard array
+// otherwise, let Arrow try to use copy-less subarray call
+function arrayFromVector(vector) {
+  return vector.dictionary ? arrayFromDictionary(vector)
+    : vector.nullCount > 0 ? [...vector]
+    : vector.toArray();
+}
 
-  // populate array values
-  let i = -1;
-  for (const chunk of chunks) {
-    const { nullBitmap: nulls, data: { values, length: m } } = chunk;
-    if (nulls && nulls.length) {
-      for (let j = 0; j < m; ++j) {
-        // j >> 3 advances the byte every 8 bits;
-        // (1 << (j & 7) checks if the relevant bit is set in that byte
-        array[++i] = !(nulls[j >> 3] & (1 << (j & 7)))
-          ? null
-          : dict[values[j]];
-      }
-    } else {
-      for (let j = 0; j < m; ++j) {
-        array[++i] = dict[values[j]];
-      }
+// produce array of decoded dictionary values
+function arrayFromDictionary(vector) {
+  const lookup = dictionaryLookup(vector);
+  const array = Array(vector.length);
+  let row = -1;
+  for (const chunk of vector.chunks) {
+    const extract = extractFromDictionary(chunk, lookup);
+    const len = chunk.length;
+    for (let idx = 0; idx < len; ++idx) {
+      array[++row] = extract(idx);
     }
   }
-
   return array;
 }
