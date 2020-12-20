260a1a80abd60c57a42a8b62fc0b24ec4299942e
jheer <jeffrey.heer@gmail.com>
2020-12-20 22:40:06 +0100

    refactor: Streamline dedupe implementation.

--- src/util/unique-name.js
+++
@@ -1,9 +0,0 @@
-export default function(names, prefix) {
-  const set = new Set(names);
-  let name = prefix;
-  let index = 0;
-  while (set.has(name)) {
-    name = `${prefix}_${++index}`;
-  }
-  return name;
-}
--- src/verbs/dedupe.js
+++ src/verbs/dedupe.js
@@ -1,14 +1,6 @@
-import uniqueName from '../util/unique-name';
-
 export default function(table, keys = []) {
-  const names = table.columnNames();
-  const num = uniqueName(names, '_num');
-  keys = keys.length ? keys : names;
-
   return table
-    .groupby(keys)
-    .derive({ [num]: 'row_number()' })
-    .filter(`d.${num} === 1`)
-    .select(names)
+    .groupby(keys.length ? keys : table.columnNames())
+    .filter('row_number() === 1')
     .reify();
 }
