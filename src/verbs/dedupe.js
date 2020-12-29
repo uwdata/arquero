9b2c46a431539092580d7d8e50e5d141b4b383bd
jheer <jeffrey.heer@gmail.com>
2020-12-29 13:19:21 +0100

    fix: Revert internal row_number use.

--- src/verbs/dedupe.js
+++ src/verbs/dedupe.js
@@ -1,8 +1,6 @@
-import { row_number } from '../op/op-api';
-
 export default function(table, keys = []) {
   return table
     .groupby(keys.length ? keys : table.columnNames())
-    .filter(() => row_number() === 1)
+    .filter('row_number() === 1')
     .reify();
 }
