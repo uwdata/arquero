34b6dca6e552e4a1d094b8ca01b03e0b05b2f5e0
jheer <jeffrey.heer@gmail.com>
2020-12-29 09:23:57 +0100

    fix: Check input types to sequence functions.

--- src/op/functions/array.js
+++ src/op/functions/array.js
@@ -1,13 +1,20 @@
+import isArray from '../../util/is-array';
+import isString from '../../util/is-string';
+import isTypedArray from '../../util/is-typed-array';
 import isValid from '../../util/is-valid';
 
+const Arrays = (seq) => isArray(seq) || isTypedArray(seq) ? seq : null;
+const Seq = (seq) => Arrays(seq) || (isString(seq) ? seq : null);
+
 export default {
-  concat:      (...values) => [].concat(...values),
-  join:        (array, delim) => array.join(delim),
-  includes:    (seq, value, index) => seq.includes(value, index),
-  indexof:     (seq, value) => seq.indexOf(value),
-  lastindexof: (seq, value) => seq.lastIndexOf(value),
-  length:      (seq) => seq.length,
-  pluck:       (array, property) => array.map(v => isValid(v)? v[property] : undefined),
-  slice:       (seq, start, end) => seq.slice(start, end),
-  reverse:     (array) => array.slice().reverse()
+  concat:       (...values) => [].concat(...values),
+  includes:     (seq, value, index) => Seq(seq).includes(value, index),
+  indexof:      (seq, value) => Seq(seq).indexOf(value),
+  join:         (array, delim) => Arrays(array).join(delim),
+  lastindexof:  (seq, value) => Seq(seq).lastIndexOf(value),
+  length:       (seq) => seq.length,
+  pluck:        (array, property) => Arrays(array)
+                  .map(v => isValid(v)? v[property] : undefined),
+  reverse:      (array) => Arrays(array).slice().reverse(),
+  slice:        (seq, start, end) => Seq(seq).slice(start, end)
 };
