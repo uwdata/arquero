f98310cb7ed8d3ee1f180e6f22a95ca63679a16f
jheer <jeffrey.heer@gmail.com>
2020-12-29 10:05:14 +0100

    fix: Safeguard code generation.

--- src/expression/compile.js
+++ src/expression/compile.js
@@ -1,13 +1,13 @@
 import { functions } from '../op';
 
 function compile(code, params) {
-  code = `"use strict"; return ${code}`;
+  code = `"use strict"; return ${code};`;
   return (Function('fn', '$', code))(functions, params);
 }
 
 export default {
-  expr:  (expr, params) => compile(`(row,data,op)=>${expr};`, params),
-  expr2: (expr, params) => compile(`(row0,data0,row,data)=>${expr};`, params),
-  join:  (expr, params) => compile(`(row1,data1,row2,data2)=>${expr};`, params),
+  expr:  (expr, params) => compile(`(row,data,op)=>${expr}`, params),
+  expr2: (expr, params) => compile(`(row0,data0,row,data)=>${expr}`, params),
+  join:  (expr, params) => compile(`(row1,data1,row2,data2)=>${expr}`, params),
   param: (expr, params) => compile(expr, params)
 };
