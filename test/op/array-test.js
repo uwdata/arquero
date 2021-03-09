889ba23e7c78fa90edb5ebfde239bfc26fee99ce
jheer <jeffrey.heer@gmail.com>
2021-03-09 18:50:50 +0100

    fix: Fix linting.

--- test/op/array-test.js
+++ test/op/array-test.js
@@ -6,7 +6,7 @@ tape('op.compact compacts an array', t => {
     [
       op.compact(Float64Array.of(1, NaN, 2)),
       op.compact([ 1, 2, 3 ]),
-      op.compact([ 1, null, 2, undefined, NaN, 3 ]),
+      op.compact([ 1, null, 2, undefined, NaN, 3 ])
     ],
     [
       Float64Array.of(1, 2),
