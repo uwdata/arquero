742183cbf37fbeb0ab39b6edd2ca2b56e9900686
jheer <jeffrey.heer@gmail.com>
2021-03-22 15:15:56 +0100

    feat: Add shuffle option for sample.

--- src/engine/sample.js
+++ src/engine/sample.js
@@ -1,7 +1,8 @@
 import sample from '../util/sample';
+import _shuffle from '../util/shuffle';
 
 export default function(table, size, weight, options = {}) {
-  const replace = !!options.replace;
+  const { replace, shuffle } = options;
   const parts = table.partitions(false);
 
   let total = 0;
@@ -26,6 +27,12 @@ export default function(table, size, weight, options = {}) {
       sample(buf, replace, idx, weight);
     }
   });
+
+  if (shuffle !== false && (parts.length > 1 || !replace)) {
+    // sampling with replacement methods shuffle, so in
+    // that case a single partition is already good to go
+    _shuffle(samples);
+  }
 
   return table.reify(samples);
 }
