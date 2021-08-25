7f2633ce97aec56eb7218fd31861e17bbca58979
Nicholas Roberts <nicholas.roberts.au@gmail.com>
2021-08-25 16:19:30 +1000

    Also pass through opt to all ast<Type> functions (requiring a few map(func) -> map(dummyVar => func(dummyVar, opt)) refactors)

--- src/query/to-ast.js
+++ src/query/to-ast.js
@@ -100,57 +100,57 @@ function astExprObject(obj, opt) {
     }));
 }
 
-function astSelection(sel) {
+function astSelection(sel, opt) {
   const type = Selection;
   return sel.all ? { type, operator: 'all' }
-    : sel.not ? { type, operator: 'not', arguments: astExprList(sel.not) }
-    : sel.range ? { type, operator: 'range', arguments: astExprList(sel.range) }
+    : sel.not ? { type, operator: 'not', arguments: astExprList(sel.not, opt) }
+    : sel.range ? { type, operator: 'range', arguments: astExprList(sel.range, opt) }
     : sel.matches ? { type, operator: 'matches', arguments: sel.matches }
     : error('Invalid input');
 }
 
-function astSelectionList(arr) {
-  return toArray(arr).map(astSelectionItem).flat();
+function astSelectionList(arr, opt) {
+  return toArray(arr).map(item => astSelectionItem(item, opt)).flat();
 }
 
-function astSelectionItem(val) {
-  return isSelection(val) ? astSelection(val)
-    : isNumber(val) ? astColumnIndex(val)
-    : isString(val) ? astColumn(val)
+function astSelectionItem(val, opt) {
+  return isSelection(val) ? astSelection(val, opt)
+    : isNumber(val) ? astColumnIndex(val, opt)
+    : isString(val) ? astColumn(val, opt)
     : isObject(val) ? Object.keys(val)
       .map(name => ({ type: Column, name, as: val[name] }))
     : error('Invalid input');
 }
 
-function astExpr(val) {
-  return isSelection(val) ? astSelection(val)
-    : isNumber(val) ? astColumnIndex(val)
-    : isString(val) ? astColumn(val)
-    : isObject(val) ? astExprObject(val)
+function astExpr(val, opt) {
+  return isSelection(val) ? astSelection(val, opt)
+    : isNumber(val) ? astColumnIndex(val, opt)
+    : isString(val) ? astColumn(val, opt)
+    : isObject(val) ? astExprObject(val, opt)
     : error('Invalid input');
 }
 
-function astExprList(arr) {
-  return toArray(arr).map(astExpr).flat();
+function astExprList(arr, opt) {
+  return toArray(arr).map(val => astExpr(val, opt)).flat();
 }
 
-function astExprNumber(val) {
-  return isNumber(val) ? val : astExprObject(val);
+function astExprNumber(val, opt) {
+  return isNumber(val) ? val : astExprObject(val, opt);
 }
 
-function astJoinKeys(val) {
+function astJoinKeys(val, opt) {
   return isArray(val)
-    ? val.map(astExprList)
-    : astExprObject(val, { join: true });
+    ? val.map(subVal => astExprList(subVal, opt))
+    : astExprObject(val, { ...opt, join: true });
 }
 
-function astJoinValues(val) {
+function astJoinValues(val, opt) {
   return isArray(val)
     ? val.map((v, i) => i < 2
         ? astExprList(v)
-        : astExprObject(v, { join: true })
+        : astExprObject(v, { ...opt, join: true })
       )
-    : astExprObject(val, { join: true });
+    : astExprObject(val, { ...opt, join: true });
 }
 
 function astTableRef(value) {
