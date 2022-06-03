1c8f438df033026535e1c4f67b4f891fa6f8f29e
jheer <jeffrey.heer@gmail.com>
2022-06-03 09:13:00 -0700

    chore: Bump minor version, update dependencies.

--- src/arrow/arrow-types.js
+++ src/arrow/arrow-types.js
@@ -1,4 +1,4 @@
-// Hardwire specific Arrow type ids to sidestep dependency
+// Hardwire Arrow type ids to sidestep dependency
 // https://github.com/apache/arrow/blob/master/js/src/enum.ts
 
 export const isDict = ({ typeId }) => typeId === -1;
