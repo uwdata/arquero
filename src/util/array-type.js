364cb99f4d028f7d0f12a062cfd9b979b02aba27
jheer <jeffrey.heer@gmail.com>
2024-07-04 19:58:26 +0200

    fix: Fix column arrayType method.

--- src/util/array-type.js
+++ src/util/array-type.js
@@ -1,5 +1,5 @@
 import isTypedArray from './is-typed-array.js';
 
 export default function(column) {
-  return isTypedArray(column.data) ? column.data.constructor : Array;
+  return isTypedArray(column) ? column.constructor : Array;
 }
